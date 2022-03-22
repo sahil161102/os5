3cb82c6fdff92915d052e84e2829225bf3d4c60f
sahil161102 <85942526+sahil161102@users.noreply.github.com>
2022-03-22 12:04:00 +0530

    Add files via upload

---
+++ 5_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<pthread.h>
+int a =5;
+
+void *increment_func( ){
+a++;
+printf("a = %d\n",a);
+}
+void *decrement_func(){
+a--;
+printf("a = %d\n",a);
+}
+
+int main(){
+int result=1;
+pthread_t t1;
+pthread_t t2;
+
+result = pthread_create(&t1,NULL,increment_func(),NULL);
+if(result)
+printf("thread1Not created");
+result = pthread_create(&t2,NULL,decrement_func(),NULL);
+if(result)
+printf("thread2Not created");
+}
+
